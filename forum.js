eb1da509409bf3aba10067ec9793e049c6f89af2
Leonmmcoset <testmod@qq.com>
2025-06-01 16:03:37 +0800

    0.4.2

--- forum.js
+++ forum.js
@@ -9,11 +9,13 @@ app.initializers.add('leonmmcoset-iframe', () => {
   
   // 扩展编辑器，添加按钮
   extend(ComposerBody.prototype, 'toolbarItems', function(items) {
+    console.log("IframeModal ready.")
     items.add('iframe', <Button 
       icon="fas fa-external-link-alt" 
       onclick={() => app.modal.show(new IframeModal())}
     >
       {app.translator.trans('flarum-iframe.forum.composer.embed_iframe_button')}
     </Button>);
+    console.log("IframeModal load done.")
   });
 });  
